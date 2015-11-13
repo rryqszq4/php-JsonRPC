1141668c48519091bf5e9a26779d5e8b1aa0c3d6
rryqszq4 <279664262@163.com>
2015-11-13 19:20:42 +0800

    [server] fix bug error: expected declaration

--- jsr_server.h
+++ jsr_server.h
@@ -40,7 +40,7 @@ PHP_METHOD(jsonrpc_server, executeprocedure);
 PHP_METHOD(jsonrpc_server, executecallback);
 PHP_METHOD(jsonrpc_server, getresponse);
 
-void jsonrpc_server_init(TSRMLS_DC);
+void jsonrpc_server_init(void TSRMLS_DC);
 
 #endif
 
